3af9cd39109e5e8a702e00b23c95c79e306548a1
Aleksandra Borelowska <al.borelowska@gmail.com>
2022-11-29 22:09:13 +0100

    first array added

--- js/script.js
+++ js/script.js
@@ -8,5 +8,9 @@ welcome();
     const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
 console.log(shopping);
 
+const product = ["oliwa, czaosnek, parmezan, dobry-makaron, pietruszka"];
+
+console.log(product);
+
 
 

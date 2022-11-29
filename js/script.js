6cd8b52b43d1c8aea5d66178f30cbe7c9b01e638
Aleksandra Borelowska <al.borelowska@gmail.com>
2022-11-29 22:48:26 +0100

    forEach

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ welcome();
 const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
 console.log(shopping);
 
-const products = ["oliwa, czaosnek, parmezan, dobry-makaron, pietruszka"];
+const products = ["oliwa", "czosnek", "parmezan", "dobry-makaron", "pietruszka"];
 
 products.forEach((product, index) => {
-    console.log(`${index +1}. ${product}`);
+    console.log(`${index + 1}. ${product}`);
 });

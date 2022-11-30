0820e41a22950689ad19f8fdc11d0f41cca3b01c
Aleksandra Borelowska <al.borelowska@gmail.com>
2022-11-30 22:13:50 +0100

    function render

--- js/script.js
+++ js/script.js
@@ -13,3 +13,37 @@ const products = ["oliwa", "czosnek", "parmezan", "dobry-makaron", "pietruszka"]
 products.forEach((product, index) => {
     console.log(`${index + 1}. ${product}`);
 });
+
+{
+    const tasks = [
+        {
+            content: "zrobić pranie",
+            done: false,
+        },
+        {
+            content: "ugotować obiad",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of task) {
+            htmlString += `
+            <li>
+              ${task.content}
+            </li>
+            `;
+        }
+        
+        document.querySelector(".js-list").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+
+    };
+    init();
+}
+

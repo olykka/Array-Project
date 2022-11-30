d4a0ba00219ca5acaa157593ce2a43f904d4a1d1
Aleksandra Borelowska <al.borelowska@gmail.com>
2022-11-30 23:43:34 +0100

    onFormSubmit

--- js/script.js
+++ js/script.js
@@ -39,23 +39,24 @@ welcome();
         render();
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-            if (newTaskContent === "") {
-                return;
-            }
-
-            addNewTask(newTaskContent);
-        });
-
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();

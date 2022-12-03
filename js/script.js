3f35403531b02d4c0134f049d5222b4b0536d1d3
Aleksandra Borelowska <al.borelowska@gmail.com>
2022-12-03 22:51:31 +0100

    task done buttons

--- js/script.js
+++ js/script.js
@@ -30,12 +30,18 @@ welcome();
         render();
     }
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+            <button class="js-done">zriobione?</button>
             <button class="js-remove">usuń</button>
               ${task.content}
             </li>
@@ -48,6 +54,13 @@ welcome();
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
